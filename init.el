e8669e7b944059f021a74b1cee2ac2123a4c1429
Randy Ridenour <rlridenour@gmail.com>
2026-07-21 06:07:47 -0500

    Add init.el to ignored dashboard files.

--- init.el
+++ init.el
@@ -3345,7 +3345,7 @@ and convert it to Org using the pandoc utility."
 (bind-key "s-d" 'randy-dashboard-open)
 
 (setq randy-dashboard-recent-files-exclude
-	'("/emacs/config.org" "tasks.org" "events.org" "calendar-beorg.org" "lib.typ" "typst.toml" "index"))
+	'("/emacs/config.org" "tasks.org" "events.org" "calendar-beorg.org" "lib.typ" "typst.toml" "index" "init.el"))
 
 (defun dashboard-startup ()
   (randy-dashboard-open)
